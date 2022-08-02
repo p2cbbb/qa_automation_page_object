048974daffee4ef37f85f6a78951753d673594c6
Denis Antonov <den69ant@yandex.ru>
2022-08-02 15:37:36 +0300

    LoginPage POM was successfully implemented

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from conftest import browser
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.url, "This is not login url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -8,10 +9,15 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                     # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
-
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
 
